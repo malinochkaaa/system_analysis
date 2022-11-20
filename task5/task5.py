fe3446c109f6467f6af0b2752fc6e481cc857400
Alina Akimova <m1905383@edu.misis.ru>
2022-11-20 17:43:49 +0300

    task5 changed

--- task5/task5.py
+++ task5/task5.py
@@ -1,7 +1,10 @@
 import json
 import numpy as np
 
-def task(ranking1, ranking2):
+def task(ranking1_str, ranking2_str):
+    ranking1 = json.loads(ranking1_str)
+    ranking2 = json.loads(ranking2_str)
+
     y_a = relationship_matrix(ranking1)
     y_a_t = y_a.transpose()
 
@@ -16,10 +19,10 @@ def task(ranking1, ranking2):
     for i in range(y_a_b.shape[0]):
         for j in range(y_a_b[i].shape[1]):
             if int(y_a_b[i,j]) == 0 and int(y_a_b_t[i,j]) == 0:
-                if (str(j+1),str(i+1)) not in conflicts:
-                    conflicts.append((str(i+1),str(j+1)))
+                if [str(j+1),str(i+1)] not in conflicts:
+                    conflicts.append([str(i+1),str(j+1)])
 
-    return json.dumps(conflicts)
+    return conflicts
 
 
 def relationship_matrix(ranking):
@@ -44,8 +47,6 @@ def ranking_length(ranking) -> int:
     return length
 
 if __name__ == '__main__':
-    str1 = ["1", ["2", "3"], "4", ["5", "6", "7"], "8", "9", "10"]
-    str2 = [["1", "2"], ["3", "4", "5"], "6", "7", "9", ["8", "10"]]
-    b1 = json.dumps(str1)
-    b2 = json.dumps(str2)
-    print(task(json.loads(b1), json.loads(b2)))
+    str1 = '["1", ["2", "3"], "4", ["5", "6", "7"], "8", "9", "10"]'
+    str2 = '[["1", "2"], ["3", "4", "5"], "6", "7", "9", ["8", "10"]]'
+    print(task(str1, str2))
