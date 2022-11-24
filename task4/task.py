7b90a08b5cbdecea6f6ef2937a95ee48a6c2a06a
Alina Akimova <m1905383@edu.misis.ru>
2022-11-24 10:49:01 +0300

    task4 fixed

--- task4/task.py
+++
@@ -1,8 +0,0 @@
-import task4
-
-reference = [['1', '2'], ['1', '3'], ['2', '3'], ['2', '4']]
-
-with open("/Users/alinaakimova/Downloads/System_analysis_Lab4-master/data.csv") as file:
-    csvString = file.read()
-    result = task4.task(csvString)
-    print(result)
--- task4/task4.py
+++ task4/task4.py
@@ -1,34 +1,41 @@
-from io import StringIO
-import math
+import math as m
 import csv
+from io import StringIO
 
 
 def task(csvString):
-    f = StringIO(csvString)
-    reader = csv.reader(f, delimiter=',')
+
+    fileString = StringIO(csvString)
+    file = csv.reader(fileString, delimiter=',')
+
     out = []
-    for row in reader:
+    for row in file:
         out.append(row)
+
     arr1 = []
     [arr1.append(i[0]) for i in out]
     arr2 = []
     [arr2.append(i[1]) for i in out]
+
     arr3 = []
     for i in range(len(out)):
         for j in range(i+1, len(out)):
             if out[i][1] == out[j][0]:
                 arr3.append(out[i][0])
+
     arr4 = []
     for i in range(len(out)):
         for j in range(i+1, len(out)):
             if out[i][1] == out[j][0]:
                 arr4.append(out[j][1])
+
     arr5 = []
     for i in range(len(out)):
         for j in range(i+1, len(out)):
             if out[i][0] == out[j][0]:
                 arr5.append(out[i][1])
                 arr5.append(out[j][1])
+
     res = []
     v = set()
     for i in out:
@@ -36,17 +43,24 @@ def task(csvString):
             v.add(int(j))
     vmax = max(v)
     v = sorted(v)
-    [res.append([]) for i in v]
+
     for i in v:
+        res.append([])
         res[i - 1].append(arr1.count(str(i)))
         res[i - 1].append(arr2.count(str(i)))
         res[i - 1].append(arr3.count(str(i)))
         res[i - 1].append(arr4.count(str(i)))
         res[i - 1].append(arr5.count(str(i)))
+
     h = 0
     for i in range(len(v)):
         for j in range(int(vmax)):
             if res[i][j] != 0:
-                h += res[i][j] * math.log(res[i][j] / (len(v) - 1), 2) / (len(v) - 1)
-    h = -h
-    return h
+                h += res[i][j] * m.log(res[i][j] / (len(v) - 1), 2) / (len(v) - 1)
+              
+    return -h
+
+if __name__ == "__main__":
+    with open("/Users/alinaakimova/Downloads/System_analysis_Lab4-master/data.csv") as file:
+        csvString = file.read()
+        print(task(csvString))
